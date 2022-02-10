db49c9425f561f39d79fb3f996793513c1e41621
RitikSisodiy <ritik.s10120@gmail.com>
2022-02-10 23:48:03 +0530

    fix

--- sequal/settings.py
+++ sequal/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     'default':{
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'sequalpackge',
         'USER': 'sequal', 
         'PASSWORD': 'Flax@2021',
