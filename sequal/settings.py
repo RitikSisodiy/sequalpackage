1c00114b9778b8c3a8af0786548b82088198b873
RitikSisodiy <ritik.s10120@gmail.com>
2022-02-11 14:36:24 +0530

    fix

--- sequal/settings.py
+++ sequal/settings.py
@@ -99,18 +99,18 @@ WSGI_APPLICATION = 'sequal.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default':{
-    #     'ENGINE': 'mysql.connector.django',
-    #     'NAME': 'sequalpackge',
-    #     'USER': 'sequal', 
-    #     'PASSWORD': 'Flax@2021',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default':{
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'sequalpackge',
+        'USER': 'sequal', 
+        'PASSWORD': 'Flax@2021',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
 
